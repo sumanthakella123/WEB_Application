69deab4fe5a5fc3b6ce5a942c0fc86d0cb711eab
DCSR6667 <sekharc446@example.com>
2024-09-09 15:30:00 -0400

    cleaning the repository

--- server/router.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {get_dieties}=require('./controllers/get_dieties');
-
-router.get("/get_dieties",get_dieties);
-
-module.exports=router;
--- src/components/VastraSeva/Diety/AddToCart.js
+++
@@ -1,9 +0,0 @@
-const AddToCart=()=>
-{
-    return (
-        <h1>this is add to cart</h1>
-    )
-
-};
-
-export default AddToCart;
