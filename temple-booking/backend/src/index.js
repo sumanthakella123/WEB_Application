c83e013c45eca40b7664c0534f2d991cfb0025b0
sumanthakella123 <akellasumanth@gmail.com>
2024-09-15 08:27:24 -0400

    committing call handler initial setup

---
+++ temple-booking/backend/src/index.js
@@ -0,0 +1,62 @@
+import Fastify from 'fastify';
+import WebSocket from 'ws';
+import dotenv from 'dotenv';
+import fastifyFormBody from '@fastify/formbody';
+import fastifyWs from '@fastify/websocket';
+
+// Load environment variables from .env file
+dotenv.config();
+
+//Retrieve the OpenAI API key from environment variables.
+const { OPENAI_API_KEY } = process.env;
+
+if (!OPENAI_API_KEY) {
+    console.error('Missing OpenAI API key. Please set it in the .env file.');
+    process.exit(1);
+}
+
+// Initialize Fastify
+const fastify = Fastify();
+fastify.register(fastifyFormBody);
+fastify.register(fastifyWs);
+
+// Constants
+const SYSTEM_MESSAGE = 'You are Neela, a active phone call assistant from the Albany Hindu Temple in Albany, NY. You are fluent in English, Hindi, Telugu, and Tamil, and assist callers with temple information, Try to give answers in few words and no more than 2 scentences,puja bookings, and event details. Your responses are respectful, realistic, and concise, guiding devotees smoothly through the booking process, gathering details like puja name, date, devotee’s name, and email sequentially. You also offer blessings and support, creating a comforting and uplifting experience for callers.'
+'Do not tell to contact us, The diwali festival celebrations is on 1st November will start at 5:00 PM and end by 9:00PM, there will be fire works and food.';
+const VOICE = 'alloy';
+const PORT = process.env.PORT || 5000; // Allow dynamic port assignment
+
+// List of Event Types to log to the console. See the OpenAI Realtime API Documentation: https://platform.openai.com/docs/api-reference/realtime
+const LOG_EVENT_TYPES = [
+    'error',
+    'response.content.done',
+    'rate_limits.updated',
+    'response.done',
+    'input_audio_buffer.committed',
+    'input_audio_buffer.speech_stopped',
+    'input_audio_buffer.speech_started',
+    'session.created'
+];
+
+// Show AI response elapsed timing calculations
+const SHOW_TIMING_MATH = false;
+
+// Root Route
+fastify.get('/', async (request, reply) => {
+    reply.send({ message: 'Albany Hindu Temple AI Server is running!' });
+});
+
+// Route for Twilio to handle incoming calls
+// <Say> punctuation to improve text-to-speech translation
+fastify.all('/incoming-call', async (request, reply) => {
+    const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
+                          <Response>
+                              <Connect>
+                                  <Stream url="wss://${request.headers.host}/media-stream" />
+                              </Connect>
+                          </Response>`;
+
+    reply.type('text/xml').send(twimlResponse);
+});
+
+
