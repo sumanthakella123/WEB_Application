6811e8a72180aabbdbf0c9f57cf980a0a4ce8845
P Tapasya <tputheti@albany.edu>
2024-10-01 19:55:54 -0400

    implemented-home-page

---
+++ src/components/AboutUs.js
@@ -0,0 +1,15 @@
+import HomeHeader from "./HomeHeader";
+
+const AboutUs=()=>
+    {
+        return (
+            <div>
+                <HomeHeader/>
+                <h1>About Us</h1>
+            </div>
+
+        )
+        
+    }
+    
+    export default AboutUs;
