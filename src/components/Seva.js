0d7e12bbd64acd010df691e9a0fb236189ff5a1d
DCSR6667 <sekharc446@example.com>
2024-10-01 19:12:15 -0400

    implemented-seva-page

---
+++ src/components/Seva.js
@@ -0,0 +1,17 @@
+import SevaHeader from './SevaHeader';
+import Footer from './Footer';
+import SevaForm from './SevaForm';
+const Seva=()=>
+{
+    return (
+        <div>
+            <SevaHeader/>
+            <SevaForm/>
+            <Footer/>
+
+        </div>
+    )
+
+};
+
+export default Seva;
