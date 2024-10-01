f2bd27512652be2a5eb9e6c4383ba611a0855bcc
DCSR6667 <sekharc446@example.com>
2024-10-01 19:36:47 -0400

    implemented-homepage

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import HomeHeader from './HomeHeader';
+import CauroselComponent from './CauroselComponent';
+import Footer from './Footer';
+import OnlineServices from './OnlineServices';
+const Home=()=>
+{
+    return (
+        <div>
+            <HomeHeader/>
+            <OnlineServices/>
+            {/* <CauroselComponent/> */}
+            <Footer/> 
+        </div>
+
+    )
+
+};
+
+export default  Home;
