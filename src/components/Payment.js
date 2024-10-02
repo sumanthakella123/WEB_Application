3b16b757edd0e1a1a3afb28e77c52f0d9ade9dc9
Rohithreddy Konda <rohithreddykonda@dyn-169-226-245-5.wireless.albany.edu>
2024-10-01 20:52:20 -0400

    implemented-header-services

---
+++ src/components/Payment.js
@@ -0,0 +1,19 @@
+import SevaHeader from './SevaHeader';
+import Footer from './Footer';
+import CardDetails from './CardDetails';
+const Payment=()=>
+{
+    return (
+        <div>
+
+            <SevaHeader/>
+            <CardDetails/>
+           
+        </div>
+
+
+    );
+
+};
+
+export default Payment;
