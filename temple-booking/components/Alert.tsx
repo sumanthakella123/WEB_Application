11da0a26e7e0d5e18e3d8ba8624aeeb669a70ecf
DCSR6667 <sekharc446@example.com>
2024-11-04 09:00:00 -0500

    implemented  reusable alert messages along with colors

--- temple-booking/components/Alert.tsx
+++ temple-booking/components/Alert.tsx
@@ -8,19 +8,19 @@ interface AlertProps {
   export default function Alert({ message, variant = 'info' }: AlertProps) {
     let bgColor;
   
-    // switch (variant) {
-    //   case 'success':
-    //     bgColor = 'bg-green-100 text-green-800 border-green-400';
-    //     break;
-    //   case 'error':
-    //   case 'destructive': // Treat "destructive" as an error
-    //     bgColor = 'bg-red-100 text-red-800 border-red-400';
-    //     break;
-    //   case 'info':
-    //   default:
-    //     bgColor = 'bg-blue-100 text-blue-800 border-blue-400';
-    //     break;
-    // }
+    switch (variant) {
+      case 'success':
+        bgColor = 'bg-green-100 text-green-800 border-green-400';
+        break;
+      case 'error':
+      case 'destructive': // Treat "destructive" as an error
+        bgColor = 'bg-red-100 text-red-800 border-red-400';
+        break;
+      case 'info':
+      default:
+        bgColor = 'bg-blue-100 text-blue-800 border-blue-400';
+        break;
+    }
   
     return (
       <div className={`p-4 border-l-4 ${bgColor} rounded mb-4`}>
