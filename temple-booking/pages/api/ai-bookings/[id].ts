def050c7d13739a598409a44d89a3578c7e2ee27
Chandra Sekhar Reddy Kasireddy <chandrasekharreddykasireddy@Chandras-MacBook-Air.local>
2024-11-08 07:43:00 -0500

    Adding method ruting structure

--- temple-booking/pages/api/ai-bookings/[id].ts
+++ temple-booking/pages/api/ai-bookings/[id].ts
@@ -4,5 +4,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { id } = req.query;
   const bookingId = parseInt(id as string);
 
-  res.status(405).end(`Method ${req.method} Not Allowed`);
+  switch (req.method) {
+    default:
+      res.setHeader('Allow', ['PUT', 'DELETE']);
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
